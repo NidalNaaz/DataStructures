5b398b4058530202dff829f4f7d75be293a95c14
NidalNaaz <147641097+NidalNaaz@users.noreply.github.com>
2024-03-27 18:52:59 +0530

    Create PostPreFix.h

---
+++ Stack/PostPreFix/PostPreFix.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template<typename T>
+string infixToPostfix(const string& infix);
+
+template<typename T>
+string infixToPrefix(const string& infix);
