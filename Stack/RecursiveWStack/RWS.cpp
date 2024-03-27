5c9bf25ec27d37760863d1a85f53b090f8d13105
NidalNaaz <147641097+NidalNaaz@users.noreply.github.com>
2024-03-27 19:03:17 +0530

    Create RWS.cpp

---
+++ Stack/RecursiveWStack/RWS.cpp
@@ -0,0 +1,23 @@
+#include "RWS.h"
+
+int recursiveFunction(int n) {
+    stack<int> callStack;
+
+    callStack.push(n);
+
+    int result = 1; 
+
+    while (!callStack.empty()) {
+        int currentN = callStack.top();
+        callStack.pop();
+
+        if (currentN == 0) {
+            result = 1; 
+        } else {
+            callStack.push(currentN - 1);
+            callStack.push(currentN - 1);
+        }
+    }
+
+    return result;
+}
