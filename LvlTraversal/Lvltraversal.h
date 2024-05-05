d698898dd3d219ae775f4a8d883ff84b33738bf2
NidalNaaz <147641097+NidalNaaz@users.noreply.github.com>
2024-05-05 16:12:23 +0530

    Create Lvltraversal.h

---
+++ LvlTraversal/Lvltraversal.h
@@ -0,0 +1,20 @@
+#include<iostream>
+
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int val);
+};
+
+class BinaryTree {
+private:
+    TreeNode* root;
+public:
+    BinaryTree();
+    ~BinaryTree();
+    void insert(int val);
+    void levelOrderTraversal();
+};
+
+
