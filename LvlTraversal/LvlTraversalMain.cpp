e71aaf584cb52cb06cd2d93f095c78040df4dfad
NidalNaaz <147641097+NidalNaaz@users.noreply.github.com>
2024-05-05 16:18:09 +0530

    Create LvlTraversalMain.cpp

---
+++ LvlTraversal/LvlTraversalMain.cpp
@@ -0,0 +1,14 @@
+#include "LevelOTrav.cpp"
+
+int main() {
+    BinaryTree tree;
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+    tree.insert(4);
+    tree.insert(5);
+    tree.insert(6);
+    tree.insert(7);
+    tree.levelOrderTraversal();
+    return 0;
+}
